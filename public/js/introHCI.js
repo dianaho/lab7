567bafdeea7546aae61340508199189fffe441d1
Diana Ho <dianahox3@gmail.com>
2016-02-18 22:21:45 +0000

    Fixed introHCI.js

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".btn").click(Likebutton);
+}
+
+function Likebutton(e) {
+	e.preventDefault();
+	ga('send', 'event', 'like', 'click');
 }
