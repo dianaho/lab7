ab4827aa18b13d18f0325a633fd5c5f03100abed
Diana Ho <dianahox3@gmail.com>
2016-02-18 21:21:47 +0000

    First push

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,20 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	var random_num = Math.random();
+	console.log(random_num);
+
+	if (random_num >= 0) {
+	  projects['grid'] = false;
+	  res.render('index', projects);
+	} else {
+	  res.redirect('/grid');
+	}
+};
+
+
+exports.viewGrid = function(req, res){
   	res.render('index', projects);
+  	console.log(projects.grid);
+  	projects["grid"] = true;
 };
